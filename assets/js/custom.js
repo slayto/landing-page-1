51ccc6f754ba47eaa1a4945d0cfb96e7364c53c8
slayto <156730195+slayto@users.noreply.github.com>
2024-02-23 16:25:55 +0800

    files

---
+++ assets/js/custom.js
@@ -0,0 +1,31 @@
+$('.counter-count').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+
+// Animation
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('image-animation');
+      }
+    });
+  });
+  
+  const viewbox = document.querySelectorAll('.left-ani');
+  viewbox.forEach(image => {
+    observer.observe(image);
+  });
+
